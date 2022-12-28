e1c58d4f9f8387ee5e2cfb7ffddc548ec6be0d45
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-28 02:07:11 +0000

    added form component

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/navbar";
 import Background from "./components/Background";
+import Form from "./components/Form";
 function App() {
   return (
     <Fragment>
       <Background/>
       <Navbar/>
+      <Form/>
     </Fragment>
     
 
