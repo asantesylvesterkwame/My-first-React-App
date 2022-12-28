fbbe01919fd6c4fddb07dd9129b1d8bbdc2840ad
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-28 01:33:40 +0000

    imported Background Jsx

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Navbar from "./components/navbar";
-
+import Background from "./components/Background";
 function App() {
   return (
-    <Navbar/>
+    <Fragment>
+      <Background/>
+      <Navbar/>
+    </Fragment>
+    
+
+    
   );
 }
 
