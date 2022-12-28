35c854fb3d71efe15cf23f57fdd6200820260a47
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-28 00:53:16 +0000

    created react app files

---
+++ my-first-react-app/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar(props) {
+    return (
+        <div className="titleBar">
+            <h1>CV CREATOR</h1>
+        </div>
+        
+    );
+}
+
+export default Navbar
