697dde988438fc51b7f48d79727c67fdff433a0e
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-28 02:07:50 +0000

    imported needed files for form component

---
+++ my-first-react-app/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Form.css";
+
+function Form(props) {
+    return(
+        <div className="formDiv">
+            
+        </div>
+    );
+}
+
+export default Form;
